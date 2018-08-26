520e42ab847f0d486bdc6a82db3c5cc08b0802c5
clapertosa <the_wolf91@libero.it>
2018-08-26 20:22:41 +0200

    Set webpack.prod mode to production

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   devtool: "cheap-module-source-map",
   entry: "./src/index.js",
   output: {
