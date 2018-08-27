9842712f5cc8ae9417cb0c5111ca375fcca60e29
clapertosa <the_wolf91@libero.it>
2018-08-27 15:01:09 +0200

    Add Morgan as logger

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
+const compression = require("compression");
+const morgan = require("morgan");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
+  app.disable("x-powered-by");
+  app.use(compression());
+  app.use(morgan("common"));
+
   app.use(express.static(path.join(__dirname, "dist")));
 
-  // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
 
-app.listen(PORT, () => console.log("Listening on port", PORT));
+app.listen(PORT, () => console.log("Server started"));
